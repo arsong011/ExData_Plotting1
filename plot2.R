ed42dc5d5e535f713f05cc74594ece083a08d633
arsong011 <91659464+arsong011@users.noreply.github.com>
2021-11-21 08:13:50 +0900

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,14 @@
+## Creating plot2
+
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+plot(hpc$Time,hpc$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+
+
+## Export plot1 as png
+
+png("plot2.png", width=480, height=480)
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+plot(hpc$Time,hpc$Global_active_power, ylab="Global Active Power (kilowatts)", 
+     xlab="", pch =".", type="l")
+dev.off()
